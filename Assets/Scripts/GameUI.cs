5e6e3ce78a146a994ac7f3ce47796b5bd76e5040
João Freire <jsfoao@gmail.com>
2020-10-20 12:23:41 +0200

    Game saving

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -1,15 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameUI : MonoBehaviour
 {
-    public GameObject goldText;
-    public GameObject pressesText;
-    public GameObject buyButton;
-    public GameObject sellButton;
+    public PlayerManager playerManager;
+    public Text goldText;
+    public Text pressesText;
+    public Button buyButton;
+    public Button sellButton;
 
-    private void Start()
+    private void Update()
     {
+        goldText.text = "Gold: " + playerManager.gold + " (" + playerManager.goldPerSecond + ")"; ;
+        pressesText.text = "Presses: " + playerManager.totalPresses;
+
+        //button colors
+        if(playerManager.gold < playerManager.pressBuyPrice)
+        {
+            ChangeButtonColor(buyButton, Color.red);
+        }
+        else
+        {
+            ChangeButtonColor(buyButton, Color.green);
+        }
+    }
+
+    private void ChangeButtonColor(Button _button, Color _newcolor)
+    {
+        ColorBlock colors = _button.colors;
+        colors.normalColor = _newcolor;
+        _button.colors = colors;
     }
 }
---
+++ Assets/Scripts/SaveManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveManager : MonoBehaviour
+{
+	public PlayerManager playerManager;
+
+    private void Start()
+    {
+		playerManager = GetComponent<PlayerManager>();
+    }
+    public void SaveGame()
+	{
+		PlayerPrefs.SetFloat("gold", playerManager.gold);
+		PlayerPrefs.SetFloat("presses", playerManager.totalPresses);
+		PlayerPrefs.Save();
+		Debug.Log("Game data saved!");
+	}
+
+	public void LoadGame()
+	{
+		if (PlayerPrefs.HasKey("gold") && PlayerPrefs.HasKey("presses"))
+		{
+			playerManager.gold = PlayerPrefs.GetFloat("gold");
+			playerManager.totalPresses = PlayerPrefs.GetFloat("presses");
+			Debug.Log("Game data loaded!");
+		}
+		else
+			Debug.LogError("There is no save data!");
+	}
+
+	public void ResetData()
+	{
+		PlayerPrefs.DeleteAll();
+		playerManager.gold = 0f;
+		playerManager.totalPresses = 0f;
+		Debug.Log("Data reset complete");
+	}
+}
