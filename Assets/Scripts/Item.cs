ff44329b836e90642b40f6bb1e68a5eb96166c40
João Freire <jsfoao@gmail.com>
2020-10-21 09:49:13 +0200

    Items array

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Item : MonoBehaviour
+[System.Serializable]
+public class Item
 {
     public string itemName;
     public float cost;
     public float prodTime;
     public float prodAmount;
+    public float total;
 
     public Item(string _name, float _cost, float _prodTime, float _prodAmount)
     {
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -15,6 +15,7 @@ public class PlayerManager : MonoBehaviour
     public float pressBuyPrice;
     public float pressSellPrice;
 
+    public Item[] items;
     private float Timer;
 
     private void Update()
@@ -24,7 +25,7 @@ public class PlayerManager : MonoBehaviour
             AddGold(goldClick);
         }
 
-        GoldPerSecond();
+        Debug.Log(TotalGoldProd());
     }
 
     public void AddGold(float _gold)
@@ -37,37 +38,53 @@ public class PlayerManager : MonoBehaviour
         this.gold -= _gold;
     }
 
-    public void BuyPress()
+    public void BuyItem(Item _item)
     {
-        if (this.gold >= pressBuyPrice)
+        if (this.gold >= _item.cost)
         {
-            this.totalPresses++;
-            SubtractGold(pressBuyPrice);
+            _item.total++;
+            SubtractGold(_item.cost);
         }
         else
             Debug.Log("not enough");
     }
 
-    public void Sell()
+    public float TotalGoldProd()
     {
-        if (totalPresses > 0)
+        float totalProdAmount = 0;
+        float totalItems = 0;
+        float totalGold;
+
+        for(int i = 0; i <= items.Length; i++)
         {
-            this.totalPresses--;
-            AddGold(pressSellPrice);
+            totalProdAmount += items[i].total;
+            totalItems += items[i].total;
         }
-        else
-            Debug.Log("no presses to sell");
+
+        totalGold = totalProdAmount * totalItems;
+        return totalGold;
     }
 
     private void GoldPerSecond()
     {
-        goldPerSecond = pressIncrease * totalPresses;
-
         Timer += Time.deltaTime;
         if (Timer >= 1f)
         {
             Timer = 0f;
-            gold += goldPerSecond;
+            this.gold += this.TotalGoldProd();
         }
     }
 }
+
+/*
+ * To 14: Create a class named PurchasableProduct. It does not inherit from MonoBehaviour. Use the [System.Serializable]-Attribute. If you now create a public PurchasableProduct[] purchasableProducts; you can configure everything in the inspector.
+
+ * To 15: We will have to instantiate buttons depending on the configuration here. Useful methods and classes: 
+     var newGameObject = Object.Instantiate(gameObject)->creates a copy of the given GameObject. You could pass a 
+    reference to a prefab to this function by using a public field of type GameObject and then referencing the Prefab in the Unity Editor.
+
+     I would create a script named PurchasableProductView to put on that gameObject. 
+    So you can reference all Texts using public fields. public Text costText;public Text productName; etc.
+     Var view = newGameObject.GetComponent<PurchasableProductView>()->will then give you your view.
+     VerticalLayoutGroup -> It is a component from Unity that helps you layout multiple UI Elements above each other automatically. 
+     You can set up the width and height of each button here.*/
